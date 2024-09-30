22cd72b3d523c9cb00f6177fdd4d92e46e1503d5
Daniel Pereira do Nascimento <daniel.nascimento@etipi.pi.gov.br>
2024-09-30 10:36:38 -0300

    first commit

---
+++ src/main/java/com/backend/srv/srv_password/SrvPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.backend.srv.srv_password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/srv/srv_password/SrvPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.srv.srv_password;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
